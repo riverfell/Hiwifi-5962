a7146f7d0758e5be95889bd455129ad0d785b069
riverfell <59993421+riverfell@users.noreply.github.com>
2021-02-08 16:30:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,15 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
+
+# Modify timezone
+sed -i '/CST-8/a\\t\tset system.@system[-1].zonename=Asia/Shanghai' package/base-files/files/bin/config_generate
+sed -i "s#Asia/Shanghai#'Asia/Shanghai'#g" package/base-files/files/bin/config_generate
+
+# Change defalut theme
+sed -i "s/bootstrap/argon/g" feeds/luci/collections/luci/Makefile
+
+# 修改连接数
+echo "net.netfilter.nf_conntrack_max= 65535" >> package/base-files/files/etc/sysctl.conf
