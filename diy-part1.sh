73d1827eca79884583b7153d919847a1df6bddf6
riverfell <59993421+riverfell@users.noreply.github.com>
2021-02-08 16:29:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,12 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# add for passwall
+#echo "src-git passwall https://github.com/keramist/passwall" >> feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# Change defalut theme
+sed -i "s/bootstrap/argon/g" feeds/luci/collections/luci/Makefile
